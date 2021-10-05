8909c929223cf6aa7ff34e3fa60a513b0c0e8d31
Son Dang <sonhdang@ucla.edu>
2021-10-05 04:22:32 -0700

    Completed Anonymous functions

---
+++ anonymous-functions.js
@@ -0,0 +1,14 @@
+/* anonymous functions = function without a name. Often not
+accessible after its initial creation. Benefits: does not 
+clutter global namespace, usable once, can be passed as arguments */
+
+(function(){
+    document.getElementById("myH1").innerHTML = "hey!":
+})();
+
+/* a typical example of when this functions is used is when
+you want to pass in a function for setTimeout(function, ms) */
+
+setTimeout(
+    function(){document.getElementById("myH1").innerHTML = "sup?"}, 
+    1000);
